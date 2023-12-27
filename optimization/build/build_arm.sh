b9e6de1ad6cb2651b150b663cb8d977e2c4eb6a3
root <root@DESKTOP-WCBKZ90.localdomain>
2023-12-27 22:11:34 +0800

    增加speed_rgb2gray对应arm neon

--- optimization/build/build_arm.sh
+++ optimization/build/build_arm.sh
@@ -1,9 +1,11 @@
 
 opencv_include=/home/chenpeng/opencv-4.5.0/arm_build/install/include/opencv4
 opencv_lib=/home/chenpeng/opencv-4.5.0/arm_build/install/lib
+dst_dir=/home/chenpeng/qemu/buildroot-2023.02.8/output/target/root/build
 
 # speed_rgb2gray
 export LD_LIBRARY_PATH=$opencv_lib:$LD_LIBRARY_PATH
 obj=speed_rgb2gray_armv7
-arm-linux-gnueabihf-g++ -O2  -march=armv7 ../src/speed_rgb2gray.cpp -I $opencv_include -I $opencv_include/opencv2 -L$opencv_lib -lopencv_core -lopencv_imgproc -lopencv_imgcodecs -o ../build/$obj  
-#../build/speed_rgb2gray
+arm-linux-gnueabihf-g++ -O2 -march=armv7-a -marm -mfpu=neon ../src/speed_rgb2gray.cpp -I $opencv_include -I $opencv_include/opencv2 -DARM_NEON \
+                        -L$opencv_lib -lopencv_core -lopencv_imgproc -lopencv_imgcodecs -o ../build/$obj  
+cp ../build/$obj $dst_dir
--- optimization/src/speed_rgb2gray.cpp
+++ optimization/src/speed_rgb2gray.cpp
@@ -17,6 +17,10 @@
 #include <immintrin.h>
 #endif
 
+#ifdef ARM_NEON
+#include <arm_neon.h>
+#endif
+
 #include <opencv2/opencv.hpp>
 #include <future>
 using namespace std;
@@ -83,8 +87,8 @@ void RGB2Y_3(unsigned char *Src, unsigned char *Dest, int Width, int Height, int
 	}
 }
 
-//sse 一次处理12个
 #ifdef LINUX
+//sse 一次处理12个
 void RGB2Y_4(unsigned char *Src, unsigned char *Dest, int Width, int Height, int Stride) {
 	const int B_WT = int(0.114 * 256 + 0.5);
 	const int G_WT = int(0.587 * 256 + 0.5);
@@ -115,11 +119,11 @@ void RGB2Y_4(unsigned char *Src, unsigned char *Dest, int Width, int Height, int
 			__m128i sumaH = _mm_add_epi16(p3aH, _mm_add_epi16(p1aH, p2aH));//14
 			__m128i sumbL = _mm_add_epi16(p3bL, _mm_add_epi16(p1bL, p2bL));//15
 			__m128i sumbH = _mm_add_epi16(p3bH, _mm_add_epi16(p1bH, p2bH));//16
-			__m128i sclaL = _mm_srli_epi16(sumaL, 8);//17
+			__m128i sclaL = _mm_srli_epi16(sumaL, 8);//17 移位操作，相当于普通算法中的>>8
 			__m128i sclaH = _mm_srli_epi16(sumaH, 8);//18
 			__m128i sclbL = _mm_srli_epi16(sumbL, 8);//19
 			__m128i sclbH = _mm_srli_epi16(sumbH, 8);//20
-			__m128i shftaL = _mm_shuffle_epi8(sclaL, _mm_setr_epi8(0, 6, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1));//21
+			__m128i shftaL = _mm_shuffle_epi8(sclaL, _mm_setr_epi8(0, 6, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1));//21 当掩模值大于1时，该值除以16求余，如17即为1
 			__m128i shftaH = _mm_shuffle_epi8(sclaH, _mm_setr_epi8(-1, -1, -1, 18, 24, 30, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1));//22
 			__m128i shftbL = _mm_shuffle_epi8(sclbL, _mm_setr_epi8(-1, -1, -1, -1, -1, -1, 0, 6, 12, -1, -1, -1, -1, -1, -1, -1));//23
 			__m128i shftbH = _mm_shuffle_epi8(sclbH, _mm_setr_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, 18, 24, 30, -1, -1, -1, -1));//24
@@ -244,7 +248,7 @@ void  _RGB2Y(unsigned char* Src, const int32_t Width, const int32_t start_row, c
 
 			//0  1  2  3   4  5  6  7  8  9  10 11 12 13 14 15    16 17 18 19 20 21 22 23 24 25 26 27 28   29 30  31       
 			//B1 G1 R1 B2 G2 R2 B3 G3  B6 G6 R6 B7 G7 R7 B8 G8    R3 B4 G4 R4 B5 G5 R5 B6 R8 B9 G9 R9 B10 G10 R10 B11
-			__m256i mul = _mm256_packus_epi16(in1, in2);
+			__m256i mul = _mm256_packus_epi16(in1, in2); // 2*64位A 2*64位B 2*64位A 2*64位B
 
 			__m256i b1 = _mm256_shuffle_epi8(mul, _mm256_setr_epi8(
 				//  B1 B2 B3 -1, -1, -1  B7  B8  -1, -1, -1, -1, -1, -1, -1, -1,
@@ -323,6 +327,143 @@ void RGB2Y_7(unsigned char *Src, unsigned char *Dest, int width, int height, int
 }
 #endif
 
+#ifdef ARM_NEON
+//ARM NEON加速
+void RGB2Y_8(uint8_t *Src, uint8_t *Dest, int Width, int Height, int Stride) {
+	uint8_t B_WT = uint8_t(0.114 * 256 + 0.5);
+	uint8_t G_WT = uint8_t(0.587 * 256 + 0.5);
+	uint8_t R_WT = 256 - B_WT - G_WT; // int(0.299 * 256 + 0.5)
+	uint8x8_t rfac = vdup_n_u8(B_WT);  // 转换权值 R
+    uint8x8_t gfac = vdup_n_u8(G_WT); // 转换权值 G
+    uint8x8_t bfac = vdup_n_u8(R_WT);  // 转换权值 B
+	const int size = Width * Height;
+	int i = 0;
+	uint8_t *pstSrc = Src;
+	uint8_t *pstDest = Dest;
+	for (i = 0; i < size - 8*3; i += 8*3)
+	{
+		uint16x8_t temp;
+        // uint8x8 表示将64bit寄存器 分成 8 个 8bit
+        uint8x8x3_t rgb = vld3_u8(pstSrc); //一次读取3个unit8x8到3个64位寄存器
+        uint8x8_t result;
+ 
+        temp = vmull_u8(rgb.val[0], rfac);       // temp=rgb.val[0]*rfac
+        temp = vmlal_u8(temp, rgb.val[1], gfac); // temp=temp+rgb.val[1]*gfac
+        temp = vmlal_u8(temp, rgb.val[2], bfac); // temp=temp+rgb.val[2]*bfac
+ 
+        result = vshrn_n_u16(temp, 8); // vshrn_n_u16 会在temp做右移8 位的同时将2字节无符号型转成1字节无符号型
+        vst1_u8(pstDest, result);      // 转存运算结果到dest
+        pstSrc += 8 * 3;
+        pstDest += 8;
+	}
+	for (; i < size; i += 3) 
+	{
+		*pstDest = (B_WT * pstSrc[0] + G_WT * pstSrc[1] + R_WT * pstSrc[2]) >> 8;
+		pstSrc += 3;
+        pstDest++;
+	}
+}
+
+void RGB2Y_9(uint8_t *Src, uint8_t *Dest, int Width, int Height, int Stride)
+//void rgb2grayAssembly(const unsigned char *rgbImage,
+//                      const int            height,
+//                      const int            width,
+//                      unsigned char       *grayImage)
+{
+    int size = Height * Width;
+    const unsigned char *src = Src;
+    unsigned char *dst = Dest;
+
+    int neonLen = size >> 4;
+	uint8_t B_WT = uint8_t(0.114 * 256 + 0.5);
+	uint8_t G_WT = uint8_t(0.587 * 256 + 0.5);
+	uint8_t R_WT = 256 - B_WT - G_WT; // int(0.299 * 256 + 0.5)
+    //int remain  = size - (neonLen << 4); 
+
+#ifdef __aarch64__  // armv8
+    // pass
+#else   // armv7
+     __asm__ volatile(
+        // [rW, rW, rW, rW, rW, rW, rW, rW]
+        "vmov.u8   d0, #77                  \n"
+        // [gW, gW, gW, gW, gW, gW, gW, gW]
+        "vmov.u8   d1, #150                 \n"
+        // [bW, bW, bW, bW, bW, bW, bW, bW]
+        "vmov.u8   d2, #29                  \n"
+
+        "0:                                 \n"
+
+        "pld        [%[src], #512]          \n"
+
+        // load [rgb,rgb,rgb,rgb,rgb,rgb,rgb,rgb] into 
+        // [b,b,b,b,b,b,b,b], [g,g,g,g,g,g,g,g], [r,r,r,r,r,r,r,r]
+        "vld3.u8   {d3,d4,d5}, [%[src]]!    \n"
+        // load next [rgb,rgb,rgb,rgb,rgb,rgb,rgb,rgb] into 
+        // [b,b,b,b,b,b,b,b], [g,g,g,g,g,g,g,g], [r,r,r,r,r,r,r,r]
+        "vld3.u8   {d6,d7,d8}, [%[src]]!    \n"
+
+        // [r,r,r,r,r,r,r,r] * [rW,rW,rW,rW,rW,rW,rW,rW]
+        // bitwidth of results(uint16_t) are 
+        // wider than elements in inputs(uint8_t)
+        //"vmull.u8	q5, d3, d0              \n"
+        //"vmull.u8	q6, d6, d0              \n"
+		"vmull.u8	q5, d5, d0              \n"
+		"vmull.u8	q6, d8, d0              \n"
+
+        // [g,g,g,g,g,g,g,g] * [gW,gW,gW,gW,gW,gW,gW,gW]
+        // bitwidth of results(uint16_t) are 
+        // wider than elements in inputs(uint8_t)
+        "vmull.u8	q7, d4, d1              \n"
+        "vmull.u8	q8, d7, d1              \n"
+
+        // r*rW + [b,b,b,b,b,b,b,b] * [bW,bW,bW,bW,bW,bW,bW,bW]
+        //"vmlal.u8	q5, d5, d2              \n"
+        //"vmlal.u8	q6, d8, d2              \n"
+		"vmlal.u8	q5, d3, d2              \n"
+        "vmlal.u8	q6, d6, d2              \n"
+
+        // (r*rW + g*gW + b*bW) >> 8
+        "vaddhn.u16	d18, q5, q7             \n"
+        "vaddhn.u16	d19, q6, q8             \n"
+
+        "subs       %[neonLen], #1          \n"
+
+        "vst1.u8   {d18-d19}, [%[dst]]!     \n"
+
+        "bgt        0b                      \n"
+
+        :[src]        "+r"(src),
+         [dst]        "+r"(dst),
+         [neonLen]    "+r"(neonLen)
+        :
+        :"cc", "memory", "q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"
+    );
+#endif
+	int i = neonLen << 4;
+	uint8_t *pstSrc = Src + 3 * i;
+	uint8_t *pstDest = Dest + i;
+	for (; i < size; i += 3) 
+	{
+		*pstDest = (B_WT * pstSrc[0] + G_WT * pstSrc[1] + R_WT * pstSrc[2]) >> 8;
+		pstSrc += 3;
+        pstDest++;
+	}
+}
+#endif
+
+bool compare(unsigned char *Src, unsigned char *Dest, int width, int height)
+{
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			int pos = i * width + j;
+			if (Src[pos] - Dest[pos] > 1 || Src[pos] - Dest[pos] < -1) return false;
+		}
+	}
+	return true;
+}
+
 int main() {
     std::cout << "--------------------start speed_rgb2gray--------------------" << std::endl;
 	Mat src = imread("../image/car.jpg");
@@ -330,12 +471,13 @@ int main() {
 	int Width = src.cols;
     printf("Height:%d,Width:%d\n", Height, Width);
 	unsigned char *Src = src.data;
-	unsigned char *Dest = new unsigned char[Height * Width];
+	unsigned char *Base = (unsigned char *)malloc(Height * Width);
+	unsigned char *Dest = (unsigned char *)malloc(Height * Width);
 	int Stride = Width * 3;
 	int Radius = 11;
 	int64 st = cv::getTickCount();
 	for (int i = 0; i < 100; i++) {
-		RGB2Y(Src, Dest, Width, Height, Stride);
+		RGB2Y(Src, Base, Width, Height, Stride);
 	}
 	double duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 10;
 	printf("origin RGB2Y %.5f ms \n", duration);
@@ -346,6 +488,9 @@ int main() {
 	}
 	duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 10;
 	printf("int RGB2Y_1 %.5f ms \n", duration);
+	bool flg = compare(Base, Dest, Width, Height);
+	if (flg) printf("RGB2Y_1 correct \n");
+	else printf("RGB2Y_1 error \n");
 
     st = cv::getTickCount();
 	for (int i = 0; i < 100; i++) {
@@ -353,13 +498,21 @@ int main() {
 	}
 	duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 10;
 	printf("4路并行 RGB2Y_2 %.5f ms \n", duration);
+	flg = compare(Base, Dest, Width, Height);
+	printf("int RGB2Y_2 %.5f ms \n", duration);
+	flg = compare(Base, Dest, Width, Height);
+	if (flg) printf("RGB2Y_2 correct \n");
+	else printf("RGB2Y_2 error \n");
 
     st = cv::getTickCount();
 	for (int i = 0; i < 100; i++) {
 		RGB2Y_3(Src, Dest, Width, Height, Stride);
 	}
 	duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 10;
-	printf("openmp RGB2Y_3 %.5f ms \n", duration);
+	printf("int RGB2Y_3 %.5f ms \n", duration);
+	flg = compare(Base, Dest, Width, Height);
+	if (flg) printf("RGB2Y_3 correct \n");
+	else printf("RGB2Y_3 error \n");
 
 #ifdef LINUX
     st = cv::getTickCount();
@@ -368,6 +521,9 @@ int main() {
 	}
 	duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 10;
 	printf("sse 一次处理12个 RGB2Y_4 %.5f ms \n", duration);
+	flg = compare(Base, Dest, Width, Height);
+	if (flg) printf("RGB2Y_4 correct \n");
+	else printf("RGB2Y_4 error \n");
 
     st = cv::getTickCount();
 	for (int i = 0; i < 100; i++) {
@@ -375,6 +531,9 @@ int main() {
 	}
 	duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 10;
 	printf("sse 一次处理15个 RGB2Y_5 %.5f ms \n", duration);
+	flg = compare(Base, Dest, Width, Height);
+	if (flg) printf("RGB2Y_5 correct \n");
+	else printf("RGB2Y_5 error \n");
 
     st = cv::getTickCount();
 	for (int i = 0; i < 100; i++) {
@@ -382,6 +541,9 @@ int main() {
 	}
 	duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 10;
 	printf("avx2 RGB2Y_6 %.5f ms \n", duration);
+	flg = compare(Base, Dest, Width, Height);
+	if (flg) printf("RGB2Y_6 correct \n");
+	else printf("RGB2Y_6 error \n");
 
     st = cv::getTickCount();
 	for (int i = 0; i < 100; i++) {
@@ -389,10 +551,39 @@ int main() {
 	}
 	duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 10;
 	printf("avx2 + std::async异步编程 RGB2Y_7 %.5f ms \n", duration);
+	flg = compare(Base, Dest, Width, Height);
+	if (flg) printf("RGB2Y_7 correct \n");
+	else printf("RGB2Y_7 error \n");
 #endif
-	
+
+#ifdef ARM_NEON
+	st = cv::getTickCount();
+	for (int i = 0; i < 100; i++) {
+		RGB2Y_8(Src, Dest, Width, Height, Stride);
+	}
+	duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 10;
+	printf("ARM NEON加速 RGB2Y_8 %.5f ms \n", duration);
+	flg = compare(Base, Dest, Width, Height);
+	if (flg) printf("RGB2Y_8 correct \n");
+	else printf("RGB2Y_8 error \n");
+
+	st = cv::getTickCount();
+	for (int i = 0; i < 100; i++) {
+		RGB2Y_9(Src, Dest, Width, Height, Stride);
+	}
+	duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 10;
+	printf("内嵌汇编 RGB2Y_9 %.5f ms \n", duration);
+	flg = compare(Base, Dest, Width, Height);
+	if (flg) printf("RGB2Y_9 correct \n");
+	else printf("RGB2Y_9 error \n");
+#endif
+
 	Mat dst(Height, Width, CV_8UC1, Dest);
 	imwrite("../image/res.jpg", dst);
+	free(Base);
+	free(Dest);
+	Base = NULL;
+	Dest = NULL;
     std::cout << "--------------------end speed_rgb2gray--------------------" << std::endl;
 	return 0;
 }
